fce51cd4e3f6bcb2831bc353ef5acc77dfddb02e
carlos_jerez <chipi_4bip@hotmail.com>
2015-02-04 21:06:08 -0300

    4ta carga

---
+++ tupla.py
@@ -0,0 +1,16 @@
+#meses=('enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre')
+#cantidaddias=(31,28,31,30,31,30,31,31,30,31,30,31)
+#indice=0
+#while indice<len(meses):
+#    print meses[indice],':',cantidaddias[indice]
+#    print '\n'
+#    indice=indice+1
+
+#Definir una tupla que almacene 5 enteros. Implementar un algoritmo que imprima la suma de todos los elementos.
+numeros=(1,2,3,4,5)
+suma=0
+indice=0
+while indice<len(numeros):
+	suma=suma+numeros[indice]
+	print suma,'\n'
+	indice=indice+1
