294f2f1e238fc7c898313af2cfb49087cdd8c108
carlos_jerez <chipi_4bip@hotmail.com>
2015-02-05 14:38:58 -0300

    6ta carga

---
+++ tuplarepetitiva.py
@@ -0,0 +1,9 @@
+#Definir una tupla con 10 edades de personas. Imprimir la cantidad de personas con edades superiores a 20.
+
+
+edad=(45,34,2,56,1,15,20,30,32,40)
+nombre=('juan','katy','patricio','carlos','naty','guillermo','claudio','fernanda','camilo','araceli')
+for elemento in edad:
+    if elemento>20:
+        print elemento
+        print '\n'
