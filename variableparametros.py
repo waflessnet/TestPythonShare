71fc1c3f788b816b62a4f7f4f848984a15623c23
carlos_jerez <chipi_4bip@hotmail.com>
2015-03-07 17:07:56 -0300

    funciones con varible parametros 2do ej

--- variableparametros.py
+++ variableparametros.py
@@ -9,3 +9,18 @@ print '\n'
 print sumar(1,2,3,4)
 print '\n'
 print sumar(1,2,3,4,5,6,7,8,9,10)
+
+
+def cantidadmayor18(ed1,*edades):
+    cant=0
+    if ed1>18:
+        cant=cant+1
+    for e in edades:
+        if e>18:
+            cant=cant+1
+    return cant
+
+
+print 'Cantidad de personas mayores a 18:'
+cant=cantidadmayor18(4,56,14,67,32,21)
+print cant
